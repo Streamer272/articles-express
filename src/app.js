9ac1f9e5b94615a7c50470488605d2ec6fa43c05
Daniel Svitan <dankosaurus9@gmail.com>
2021-08-19 12:41:50 +0200

    :sparkles: Adds 404 handling

--- src/app.js
+++ src/app.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome!');
+});
+
+app.all('*', (req, res) => {
+    res.status(404).send('Invalid resource');
+});
+
+app.use(express.json());
+
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000...');
+});
