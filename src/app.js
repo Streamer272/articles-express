b744a71d670af4ca691aa138143211ab49ec2cee
Daniel Svitan <dankosaurus9@gmail.com>
2021-08-19 13:35:00 +0200

    :bug: Fixes logger

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 const express = require('express');
-const log = require('./logger');
+const { endpointHit } = require('./logger');
 const app = express();
 
 app.use(express.json());
-app.use(log);
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
     res.send('Welcome!');
+
+    next();
 });
 
-app.all('*', (req, res) => {
-    res.status(404).send('Invalid resource');
+app.all('*', (req, res, next) => {
+    if (!res.headersSent) {
+        res.status(404).send('Invalid resource');
+    }
+
+    next();
 });
+
+app.use(endpointHit);
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000...');
--- src/logger.js
+++ src/logger.js
@@ -3,16 +3,45 @@ const moment = require('moment');
 
 const fileName = `../log/log-${moment().format('DD_MM_YYYY')}.txt`;
 
-const log = (req, res, next) => {
-    let text = 'log';
-
-    console.log(text);
-    writeFileSync(fileName, `${text}\n`, {
+const writeOut = (logText) => {
+    console.log(logText);
+    writeFileSync(fileName, `${logText}\n`, {
         encoding: 'utf8',
         flag: 'a'
     });
+}
+
+const getCurrentTime = () => {
+    return moment().format('DD:MM:YYYY HH:mm:ss.SSS');
+}
+
+const getResType = (res) => {
+    if (res.status <= 200) {
+        return 'INFO';
+    }
+    else if (res.status > 200 < 400) {
+        return 'WARNING';
+    }
+    else {
+        return 'ERROR';
+    }
+}
+
+const log = (type, message) => {
+    const text = `[${type}] ${getCurrentTime()} ${message}`;
+
+    writeOut(text);
+}
+
+const endpointHit = (req, res, next) => {
+    const text = `[INFO] ${getCurrentTime()} ${res.statusCode} ${req.method} ${req.url}`;
+
+    writeOut(text);
 
     next();
 }
 
-module.exports = log;
+module.exports = {
+    log,
+    endpointHit
+};
