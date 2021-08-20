91ec6da4fb30819aae356a90c7145c6407a1ab85
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2021-08-20 18:21:59 +0200

    :sparkles: Adds database connection

--- src/app.js
+++ src/app.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
 const { endpointHit } = require('./logger');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Welcome!');
+let db;
+
+app.get('/', async (req, res, next) => {
+    const cursor = db.collection('articles').find();
+    const articles = await cursor.toArray();
+
+    res.status(200).send(articles);
+
+    next();
+});
+
+app.post('/', async (req, res, next) => {
+    try {
+        db.collection('articles').insert();
+    }
+    catch (err) {
+        res.status(400).send();
+    }
 
     next();
 });
@@ -21,6 +38,23 @@ app.all('*', (req, res, next) => {
 
 app.use(endpointHit);
 
-app.listen(5000, () => {
-    console.log('Server is listening on port 5000...');
-});
+const main = async () => {
+    const client = new MongoClient('mongodb://localhost:27017');
+    try {
+        await client.connect();
+        db = client.db('articles');
+        console.log(`Successfully connected to database ${db.databaseName}...`);
+    }
+    catch (err) {
+        console.error(`Error occurred while connecting to the database (${err})`);
+        return 1;
+    }
+
+    app.listen(5000, () => {
+        console.log('Server is running on port 5000...');
+    });
+
+    return 0;
+}
+
+main();
